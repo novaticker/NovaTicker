574b20862a6f74504a3ac947b1ab9ebaacffae96
novaticker <lds811311@gmail.com>
2025-07-12 20:00:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "NovaTicker 백엔드가 작동 중입니다!"
+    return "NovaTicker Backend Running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
