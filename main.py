0ff985b673ca5439086c015735ec95db68472ec3
novaticker <lds811311@gmail.com>
2025-07-12 19:44:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello from NovaTicker!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "NovaTicker 백엔드가 작동 중입니다!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
